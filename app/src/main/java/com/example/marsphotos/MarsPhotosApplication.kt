06a8a39a77b8c08b36e77d3ae79454eaa46664e0
Hannah <hannahsim692@gmail.com>
2024-12-02 23:28:35 -0800

    About to revert back

--- app/src/main/java/com/example/marsphotos/MarsPhotosApplication.kt
+++ app/src/main/java/com/example/marsphotos/MarsPhotosApplication.kt
@@ -19,7 +19,7 @@ import android.app.Application
 import com.example.marsphotos.data.AppContainer
 import com.example.marsphotos.data.DefaultAppContainer
 
-class MarsPhotosApplication : Application() {
+class FlickrPhotosApplication : Application() {
     /** AppContainer instance used by the rest of classes to obtain dependencies */
     lateinit var container: AppContainer
     override fun onCreate() {
